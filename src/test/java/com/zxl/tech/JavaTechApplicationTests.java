1da85ebc2ee9930005a349211255db91c98ac3c1
zhuxl <zhuxuelong@haiermoney.com>
2024-08-02 15:28:32 +0800

    feat：initialize

---
+++ src/test/java/com/zxl/tech/JavaTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxl.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
